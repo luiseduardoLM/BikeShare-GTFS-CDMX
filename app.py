b2519fd3c1cdae1c92b17325bbbd749afe28ba9a
luiseduardoLM <luiseduardo@ciencias.unam.mx>
2026-02-03 01:42:01 -0600

    Avance hasta la creación preliminar del mapa

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from helper_functions import *
 import pandas as pd 
 import streamlit as st
+import folium
+from streamlit_folium import folium_static
 
 # Se cargan los urls
 station_url = "https://gbfs.mex.lyftbikes.com/gbfs/en/station_status.json"
@@ -25,7 +27,59 @@ with col3:
     st.metric(label = "Número de anclajes vacios", value = len(df[df["num_docks_available"]>0]))
 
 with st.sidebar:
-    st.selectbox(
+    bike_method = st.selectbox(
         "¿Buscas rentar o devolverbicicleta?", 
         ("Rentar", "Devolver")
     )
+    if bike_method == "Rentar":
+        st.header("¿Donde te localizas?")
+        input_street = st.text_input("Calle", "")
+        input_city = st.text_input("Ciudad", "CDMX") # No me convence dejar este
+        input_country = st.text_input("País", "México") # No me convence dejar este
+        drive = st.checkbox("Estoy ahí") # True or False
+        findmeabike = st.button("¡Encuentrame una bici!", type="primary")
+        
+        if findmeabike:
+            if input_street != "":
+                iamhere = geocode(input_street + " " + input_city + " " + input_country)
+                if iamhere == "":
+                    st.subheader(":red[Dirección no valida]")
+            else:
+                st.subheader(":red[Dirección no valida]")
+    elif bike_method == "Devolver":
+        st.header("¿Donde te localizas?")
+        input_street = st.text_input("Calle", "")
+        input_city = st.text_input("Ciudad", "CDMX") # No me convence dejar este
+        input_country = st.text_input("País", "México") # No me convence dejar este
+        findmeadock = st.button("¡Encuentrame un anclaje!", type="primary")
+        
+        if findmeadock:
+            if input_street != "":
+                iamhere_return = geocode(input_street + " " + input_city + " " + input_country)
+                if iamhere_return == "":
+                    st.subheader(":red[Dirección no valida]")
+            else:
+                st.subheader(":red[Dirección no valida]")
+
+# Mapa
+# Creamos el mapa centrado en el angel de la independencia
+center = [19.427, -99.16771] #
+
+m = folium.Map(location=center, zoom_start=13, tiles="cartodbpositron")
+
+for _, row in df.iterrows():
+    marker_color = get_marker_color(row["num_bikes_available"])
+    folium.CircleMarker(
+        location = [row["lat"], row["lon"]],
+        radius = 2,
+        color = marker_color,
+        fill=True,
+        fill_color=marker_color,
+        fill_opacity=0.7,
+        popup=folium.Popup(f"Station ID: {row["station_id"]}<br>"
+                           f"Total Bikes Available: {row["num_bikes_available"]}<br>",
+                           max_width = 300)
+        ).add_to(m)
+    
+# Despliega el mapa
+folium_static(m)
