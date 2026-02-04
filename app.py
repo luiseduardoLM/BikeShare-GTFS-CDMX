19ba8829f913dccf583c498b8445e640eaa24e0c
luiseduardoLM <luiseduardo@ciencias.unam.mx>
2026-02-04 01:37:13 -0600

    La app esta terminada en su version beta

--- app.py
+++ app.py
@@ -25,6 +25,11 @@ with col2:
     st.metric(label = "Estaciones con bicicletas disponibles", value = len(df[df["num_bikes_available"]>0]))
 with col3:
     st.metric(label = "Número de anclajes vacios", value = len(df[df["num_docks_available"]>0]))
+    
+iamhere = 0
+iamhere_return = 0
+findmeabike = False
+findmeadock = False
 
 with st.sidebar:
     bike_method = st.selectbox(
@@ -46,40 +51,143 @@ with st.sidebar:
                     st.subheader(":red[Dirección no valida]")
             else:
                 st.subheader(":red[Dirección no valida]")
+                
     elif bike_method == "Devolver":
         st.header("¿Donde te localizas?")
-        input_street = st.text_input("Calle", "")
-        input_city = st.text_input("Ciudad", "CDMX") # No me convence dejar este
-        input_country = st.text_input("País", "México") # No me convence dejar este
+        input_street_return = st.text_input("Calle", "")
+        input_city_return = st.text_input("Ciudad", "CDMX") # No me convence dejar este
+        input_country_return = st.text_input("País", "México") # No me convence dejar este
         findmeadock = st.button("¡Encuentrame un anclaje!", type="primary")
         
         if findmeadock:
-            if input_street != "":
-                iamhere_return = geocode(input_street + " " + input_city + " " + input_country)
+            if input_street_return != "":
+                iamhere_return = geocode(input_street_return + " " + input_city_return + " " + input_country_return)
                 if iamhere_return == "":
                     st.subheader(":red[Dirección no valida]")
             else:
                 st.subheader(":red[Dirección no valida]")
 
-# Mapa
-# Creamos el mapa centrado en el angel de la independencia
-center = [19.427, -99.16771] #
+if bike_method == "Devolver" and findmeadock == False:
+    # Mapa
+    # Creamos el mapa centrado en el angel de la independencia
+    center = [19.427, -99.16771] #
+    m = folium.Map(location=center, zoom_start=13, tiles="cartodbpositron")
 
-m = folium.Map(location=center, zoom_start=13, tiles="cartodbpositron")
-
-for _, row in df.iterrows():
-    marker_color = get_marker_color(row["num_bikes_available"])
-    folium.CircleMarker(
-        location = [row["lat"], row["lon"]],
-        radius = 2,
-        color = marker_color,
-        fill=True,
-        fill_color=marker_color,
-        fill_opacity=0.7,
-        popup=folium.Popup(f"Station ID: {row["station_id"]}<br>"
-                           f"Total Bikes Available: {row["num_bikes_available"]}<br>",
-                           max_width = 300)
-        ).add_to(m)
+    for _, row in df.iterrows():
+        marker_color = get_marker_color(row["num_bikes_available"])
+        folium.CircleMarker(
+            location = [row["lat"], row["lon"]],
+            radius = 2,
+            color = marker_color,
+            fill=True,
+            fill_color=marker_color,
+            fill_opacity=0.7,
+            popup=folium.Popup(f"Station ID: {row["station_id"]}<br>"
+                            f"Total Bikes Available: {row["num_bikes_available"]}<br>",
+                            max_width = 300)
+            ).add_to(m)
+        
+    # Despliega el mapa
+    folium_static(m)
     
-# Despliega el mapa
-folium_static(m)
+
+if bike_method == "Rentar" and findmeabike == False:
+    # Mapa
+    # Creamos el mapa centrado en el angel de la independencia
+    center = [19.427, -99.16771] #
+    m = folium.Map(location=center, zoom_start=13, tiles="cartodbpositron")
+
+    for _, row in df.iterrows():
+        marker_color = get_marker_color(row["num_bikes_available"])
+        folium.CircleMarker(
+            location = [row["lat"], row["lon"]],
+            radius = 2,
+            color = marker_color,
+            fill=True,
+            fill_color=marker_color,
+            fill_opacity=0.7,
+            popup=folium.Popup(f"Station ID: {row["station_id"]}<br>"
+                            f"Total Bikes Available: {row["num_bikes_available"]}<br>",
+                            max_width = 300)
+            ).add_to(m)
+        
+    # Despliega el mapa
+    folium_static(m)
+
+if findmeabike:
+    if input_street != "":
+        if iamhere != "":
+            chosen_station = get_bike_availability(iamhere, df)  # Get bike availability (id, lat, lon)
+            center = iamhere  # Center the map on user's location
+            m1 = folium.Map(location=center, zoom_start=16, tiles='cartodbpositron')  # Create a detailed map
+            for _, row in df.iterrows():
+                marker_color = get_marker_color(row['num_bikes_available'])  # Determine marker color based on bikes available
+                folium.CircleMarker(
+                    location=[row['lat'], row['lon']],
+                    radius=2,
+                    color=marker_color,
+                    fill=True,
+                    fill_color=marker_color,
+                    fill_opacity=0.7,
+                    popup=folium.Popup(f"Station ID: {row['station_id']}<br>"
+                                       f"Total Bikes Available: {row['num_bikes_available']}<br>"
+                                       , max_width=300)
+                ).add_to(m1)
+            folium.Marker(
+                location=iamhere,
+                popup="You are here.",
+                icon=folium.Icon(color="blue", icon="person", prefix="fa")
+            ).add_to(m1)
+            folium.Marker(location=(chosen_station[1], chosen_station[2]),
+                          popup="Rent your bike here.",
+                          icon=folium.Icon(color="red", icon="bicycle", prefix="fa")
+                          ).add_to(m1)
+            coordinates, duration = run_osrm(chosen_station, iamhere)  # Get route coordinates and duration
+            folium.PolyLine(
+                locations=coordinates,
+                color="blue",
+                weight=5,
+                tooltip="it'll take you {} to get here.".format(duration),
+            ).add_to(m1)
+            folium_static(m1)  # Display the map in the Streamlit app
+            with col3:
+                st.metric(label=":green[Travel Time (min)]", value=duration)  # Display travel time
+
+if findmeadock:
+    if input_street_return != "":
+        if iamhere_return != "":
+            chosen_station = get_dock_availability(iamhere_return, df)  # Get dock availability (id, lat, lon)
+            center = iamhere_return  # Center the map on user's location
+            m1 = folium.Map(location=center, zoom_start=16, tiles='cartodbpositron')  # Create a detailed map
+            for _, row in df.iterrows():
+                marker_color = get_marker_color(row['num_bikes_available'])  # Determine marker color based on bikes available
+                folium.CircleMarker(
+                    location=[row['lat'], row['lon']],
+                    radius=2,
+                    color=marker_color,
+                    fill=True,
+                    fill_color=marker_color,
+                    fill_opacity=0.7,
+                    popup=folium.Popup(f"Station ID: {row['station_id']}<br>"
+                                       f"Total Bikes Available: {row['num_bikes_available']}<br>"
+                                       , max_width=300)
+                ).add_to(m1)
+            folium.Marker(
+                location=iamhere_return,
+                popup="You are here.",
+                icon=folium.Icon(color="blue", icon="person", prefix="fa")
+            ).add_to(m1)
+            folium.Marker(location=(chosen_station[1], chosen_station[2]),
+                          popup="Return your bike here.",
+                          icon=folium.Icon(color="red", icon="bicycle", prefix="fa")
+                          ).add_to(m1)
+            coordinates, duration = run_osrm(chosen_station, iamhere_return)  # Get route coordinates and duration
+            folium.PolyLine(
+                locations=coordinates,
+                color="blue",
+                weight=5,
+                tooltip="it'll take you {} to get here.".format(duration),
+            ).add_to(m1)
+            folium_static(m1)  # Display the map in the Streamlit app
+            with col3:
+                st.metric(label=":green[Travel Time (min)]", value=duration)  # Display travel time
